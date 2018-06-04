acecb6efb06b595489485668b0078f9b44d1f805
geckyhit <gekcy@mail.ru>
2018-06-04 22:44:37 +0300

    i was born

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import K from 'kefir'
+import morseAlph from './morse.json'
+
+const SPAN = 400
+
+const keyUps = K.fromEvents(document, 'keyup')
+const keyDowns = K.fromEvents(document, 'keydown')
+
+const spaceKeyUps = keyUps.filter((data) => data.keyCode === 32)
+const spaceKeyDowns = keyDowns.filter((data) => data.keyCode === 32)
+const enterKeyDowns = keyDowns.filter((data) => data.keyCode === 13).map(_ => "enter")
+
+const rawSignalStarts = spaceKeyDowns.map(_ => "start")
+const rawSignalEnds = spaceKeyUps.map(_ => "end")
+
+const signalStartsEnds = K.merge([rawSignalStarts, rawSignalEnds]).skipDuplicates()
+
+const signalStarts = signalStartsEnds.filter((ev) => ev === "start").map(Date.now)
+const signalEnds = signalStartsEnds.filter((ev) => ev === "end").map(Date.now)
+
+const spanStream = signalStarts.flatMapLatest((start) => signalEnds.map((end) => end - start))
+
+const dotsStream = spanStream.filter((v) => v <= SPAN).map(() => ".")
+const lineStream = spanStream.filter((v) => v > SPAN).map(() => "-")
+
+const dotsAndLines = K.merge([dotsStream, lineStream, enterKeyDowns])
+const endResult = dotsAndLines.bufferWhile(data => data !== 'enter').map(data => data.filter(str => str !== 'enter').join('')).log()
+
+endResult.observe(data => console.log(morseAlph[data]))
